7e10f52387fa6c1dcb730af4d5725cb02fc97798
Antonino-Sofia <antoninosofia420@gmail.com>
2025-04-01 18:29:44 +0200

    js-biglietto-treno

--- js/main.js
+++ js/main.js
@@ -9,3 +9,16 @@ const kmInfoElement = document.getElementById("km-info");
 const ageInfoElement = document.getElementById("age-info");
 const priceInfoElement = document.getElementById("price-info");
 const discountInfoElement = document.getElementById("discount-info");
+
+console.log(kmInfoElement);
+console.log(ageInfoElement);
+console.log(priceInfoElement);
+
+// INFO DEFAULT
+const ticketPriceKm = 0.21;
+const discountUnder = 20;
+const discounOver = 40;
+//INPUT
+const kmUser = parseFloat(kmUserElement.value);
+const ageUser = parseInt(ageUserElement.value);
+let ticketPrice = ticketPriceKm * kmUser;
