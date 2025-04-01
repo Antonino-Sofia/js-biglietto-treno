4a5927e8368cc0c196cd582531fa3fc231e1ee7c
Antonino-Sofia <antoninosofia420@gmail.com>
2025-04-01 16:33:01 +0200

    js-biglietto-treno

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// TICKET GEN
+const generateTicket = document.getElementById("generate-ticket");
+//KM
+const kmUserElement = document.getElementById("km-user");
+//AGE
+const ageUserElement = document.getElementById("age-user");
+//INFO
